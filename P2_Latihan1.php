b9657cfec82d9f3d325eabe554cc903cbd91d0f4
Akbar Juliandi <akbarju27@gmail.com>
2026-04-11 11:22:52 +0700

    Pertemuan 2

---
+++ P2_Latihan1.php
@@ -0,0 +1,14 @@
+<?php
+$a = 1;
+$b = 2;
+
+$c = $a . $b; // penggabungan string → "12"
+$d = $c + 1;  // "12" + 1 = 13
+
+echo $d . "<br>";
+
+$e = "Number";
+$f = $e . $d; // "Number13"
+
+echo $f;
+?>
---
+++ P2_Latihan3.php
@@ -0,0 +1,24 @@
+<?php
+$angka1 = 10;
+$angka2 = 5;
+
+$tambah = $angka1 + $angka2;
+$kurang = $angka1 - $angka2;
+$kali = $angka1 * $angka2;
+$bagi = $angka1 / $angka2;
+?>
+
+<html>
+<body>
+
+<h3>Perhitungan Sederhana</h3>
+
+<?php
+echo "Penjumlahan: $angka1 + $angka2 = $tambah <br>";
+echo "Pengurangan: $angka1 - $angka2 = $kurang <br>";
+echo "Perkalian: $angka1 x $angka2 = $kali <br>";
+echo "Pembagian: $angka1 / $angka2 = $bagi <br>";
+?>
+
+</body>
+</html>
