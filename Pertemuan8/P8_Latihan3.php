ee4072f8334f523053f64de5bc6318d7a55ae967
Akbar Juliandi <akbarju27@gmail.com>
2026-04-25 10:50:15 +0700

    Add files via upload

---
+++ Pertemuan8/P8_Latihan3.php
@@ -0,0 +1,16 @@
+<?php
+function repeat($text, $num = 10)
+{
+    echo "<ol>";
+
+    for($i = 0; $i < $num; $i++)
+    {
+        echo "<li>$text</li>";
+    }
+
+    echo "</ol>";
+}
+
+repeat("I'm the best", 15);
+repeat("You're the man");
+?>
