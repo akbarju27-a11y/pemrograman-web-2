40f4adb1a07d3f09ce01f699736ad701c1694d3f
Akbar Juliandi <akbarju27@gmail.com>
2026-04-25 09:27:15 +0700

    Add files via upload

---
+++ Pertemuan9/P9_Diskusi.php
@@ -0,0 +1,8 @@
+<?php
+function sapa() {
+    echo "Halo, selamat datang!";
+}
+
+// Memanggil function
+sapa();
+?>
---
+++ Pertemuan9/P9_Latihan4.php
@@ -0,0 +1,6 @@
+<?php 
+echo "Sekarang tanggal ";
+echo date('d-F-Y');
+echo "<br>dan jam ";
+echo date('h:i:s A'); 
+?>
